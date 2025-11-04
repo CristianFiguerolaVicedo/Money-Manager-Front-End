5c4649b876b1ccd73a8d76644e1c59f1068c55fd
CriFigShe <crifigshe@gmail.com>
2025-11-04 20:28:41 +0100

    signup image

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {assets} from "../assets/assets.js";
+
 const Signup = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
     return(
-        <div>Signup</div>
+        <div className="h-screen w-full relative flex items-center justify-center overflow">
+            {/*Background image with blur*/}
+            <img src={assets.login_bg} alt="Background" className="absolute inset-0 w-full h-full object-cover filter blur-sm" />
+        </div>
     )
 }
 
