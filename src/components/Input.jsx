3700159c5ea01df66de2beec45fab5f04193eaf6
CriFigShe <crifigshe@gmail.com>
2025-11-06 20:17:57 +0100

    Listing categories, add category modal and emoji picker (wip)

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,41 +1,64 @@
 import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 
-const Input = ({ label, value, onChange, placeholder, type }) => {
-    const [showPassword, setShowPassword] = useState(false);
-    const toggleShowPassword = () => {
-        setShowPassword(!showPassword);
-    }
+const Input = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+  type,
+  isSelect,
+  options,
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    return (
+  return (
     <div className="mb-4">
       <label className="text-[13px] text-slate-800 mb-1">{label}</label>
       <div className="relative">
-        <input
-          className="w-full bg-transparent outline-none border border-gray-300 rounded-md py-2 px-3 pr-10 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-          type={type === 'password' ? (showPassword ? 'text' : 'password') : type}
-          placeholder={placeholder}
-          value={value}
-          onChange={(e) => onChange(e)}
-        />
+        {isSelect ? (
+          <select
+            value={value}
+            onChange={(e) => onChange(e)}
+            className="w-full bg-tranparent outline-none border border-gray-300 rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input
+            className="w-full bg-transparent outline-none border border-gray-300 rounded-md py-2 px-3 pr-10 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+            type={
+              type === "password" ? (showPassword ? "text" : "password") : type
+            }
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => onChange(e)}
+          />
+        )}
 
-
-        {type === 'password' && (
-            <span className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer">
-                {showPassword ? (
-                    <Eye 
-                        size={20}
-                        className="text-purple-600"
-                        onClick={toggleShowPassword}
-                    />
-                ) : (
-                    <EyeOff 
-                        size={20}
-                        className="text-slate-400"
-                        onClick={toggleShowPassword}
-                    />
-                )}
-            </span>
+        {type === "password" && (
+          <span className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer">
+            {showPassword ? (
+              <Eye
+                size={20}
+                className="text-purple-600"
+                onClick={toggleShowPassword}
+              />
+            ) : (
+              <EyeOff
+                size={20}
+                className="text-slate-400"
+                onClick={toggleShowPassword}
+              />
+            )}
+          </span>
         )}
       </div>
     </div>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,16 +1,71 @@
+import { Plus } from "lucide-react";
 import Dashboard from "../components/Dashboard";
 import useUser from "../hooks/useUser";
+import CategoryList from "../components/CategoryList";
+import { useEffect, useState } from "react";
+import axiosConfig from "../util/axiosConfig";
+import { API_ENDPOINTS } from "../util/apiEndpoints";
+import toast from "react-hot-toast";
+import Modal from "../components/Modal";
+import AddCategoryForm from "../components/AddCategoryForm";
 
 const Category = () => {
-    useUser();
+  useUser();
 
-    return(
-        <Dashboard activeMenu="Category">
-            <div className="my-5 mx-auto">
-                    
-            </div>  
-        </Dashboard>
-    )
-}
+  const [loading, setLoading] = useState(false);
+  const [categoryData, setCategoryData] = useState([]);
+  const [openAddCategoryModal, setOpenAddCategoryModal] = useState(false);
+  const [openEditCategoryModal, setOpenEditCategoryModal] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const fetchCategoryDetails = async () => {
+    if (loading) {
+        return;
+    }
+
+    setLoading(true);
+
+    try {
+        const response = await axiosConfig.get(API_ENDPOINTS.GET_ALL_CATEGORIES);
+        if (response.status === 200) {
+            console.log("Categories", response.data);
+            setCategoryData(response.data);
+        }
+    } catch (error) {
+        console.error("Something went wrong. Please try again.", error);
+        toast.error(error.message);
+    } finally {
+        setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCategoryDetails();
+  }, []);
+
+  return (
+    <Dashboard activeMenu="Category">
+      <div className="my-5 mx-auto">
+        <div className="flex justify-between items-center mb-5">
+          <h2 className="text-2xl font-semibold">All Categories</h2>
+          <button onClick={() => setOpenAddCategoryModal(true)} className="flex items-center gap-1 bg-green-500/20 text-green-800 px-3 py-2 rounded-lg hover:bg-green-500/30 font-semibold hover:cursor-pointer">
+            <Plus size={15} />
+            Add Category
+          </button>
+        </div>
+
+        <CategoryList categories={categoryData} />
+
+        <Modal
+            title="Add Category"
+            isOpen={openAddCategoryModal}
+            onClose={() => setOpenAddCategoryModal(false)}
+        >
+            <AddCategoryForm />
+        </Modal>
+      </div>
+    </Dashboard>
+  );
+};
 
 export default Category;
