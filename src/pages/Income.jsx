9bc5d605281f3030c6b96db1e8f70bb5cc289ef4
CriFigShe <crifigshe@gmail.com>
2025-11-10 21:04:47 +0100

    Add income

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -5,6 +5,9 @@ import axiosConfig from "../util/axiosConfig";
 import { API_ENDPOINTS } from "../util/apiEndpoints";
 import toast from "react-hot-toast";
 import IncomeList from "../components/IncomeList";
+import Modal from "../components/Modal";
+import { Plus } from "lucide-react";
+import AddIncomeForm from "../components/AddIncomeForm";
 
 const Income = () => {
     useUser();
@@ -14,7 +17,7 @@ const Income = () => {
     const [loading, setLoading] = useState(false);
 
     const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
-    const [openDeleteeAlert, setOpenDeleteAlert] = useState({
+    const [openDeleteAlert, setOpenDeleteAlert] = useState({
         show: false,
         data: null
     });
@@ -37,8 +40,71 @@ const Income = () => {
         }
     }
 
+    const fetchIncomeCategories = async () => {
+        try {
+            const response = await axiosConfig.get(API_ENDPOINTS.CATEGORY_BY_TYPE("income"));
+            if (response.status === 200) {
+                setCategories(response.data);
+            }
+        } catch (error) {
+            console.log("Failed to fetch income categories", error);
+            toast.error(error.data?.message || "Failed to fetch income categories");
+        }
+    }
+
+    const handleAddIncome = async (income) => {
+        const {name, amount, date, icon, categoryId} = income;
+
+        if (!name.trim()) {
+            toast.error("Please enter the name");
+            return;
+        }
+
+        if (!amount || isNaN(amount) || Number(amount) <= 0) {
+            toast.error("Amount should be a valid number greater than 0");
+            return;
+        }
+
+        if (!date) {
+            toast.error("Please seleect a date");
+            return;
+        }
+
+        const todayDate = new Date().toISOString().split('T')[0];
+        if (date > todayDate) {
+            toast.error("Date cannot be future");
+            return;
+        }
+
+        if (!categoryId) {
+            toast.error("A category is required");
+            return;
+        }
+
+        try {
+            const response = await axiosConfig.post(API_ENDPOINTS.ADD_INCOME, {
+                name, 
+                amount: Number(amount), 
+                date,
+                icon,
+                categoryId
+            });
+
+            if (response.status === 201) {
+                setOpenAddIncomeModal(false);
+                toast.success("Income added successfully");
+                fetchIncomeDetails();
+                fetchIncomeCategories();
+            }
+        } catch (error) {
+            console.error('Error adding income', error);
+            toast.error(error.responsee?.data?.message || "Failed to add income");
+        }
+    }
+
     useEffect(() => {
         fetchIncomeDetails();
+        fetchIncomeCategories();
     }, []);
 
     return(
@@ -46,13 +112,26 @@ const Income = () => {
             <div className="my-5 mx-auto">
                 <div className="grid grid-cols-1 gap-6">
                     <div>
-
+                        <button onClick={() => setOpenAddIncomeModal(true)} className="flex items-center gap-1 bg-green-500/20 text-green-800 px-3 py-2 rounded-lg hover:bg-green-500/30 font-semibold hover:cursor-pointer">
+                            <Plus size={15} className="text-lg"/> Add Income
+                        </button>
                     </div>    
 
                     <IncomeList 
                     transactions={incomeData} 
                     onDelete={(id) => console.log("Deleting the income", id)}
                     />
+
+                    <Modal 
+                        isOpen={openAddIncomeModal}
+                        onClose={() => setOpenAddIncomeModal(false)}
+                        title="Add Income"
+                    >
+                        <AddIncomeForm 
+                            onAddIncome={(income) => handleAddIncome(income)}
+                            categories={categories}
+                        />
+                    </Modal>
                 </div>    
             </div>  
         </Dashboard>
