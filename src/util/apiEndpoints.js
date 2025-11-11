4db7977854c1663eedee43927c1b8d1332cc9124
CriFigShe <crifigshe@gmail.com>
2025-11-11 22:38:42 +0100

    Filter logic

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -20,5 +20,6 @@ export const API_ENDPOINTS = {
     DELETE_EXPENSE: (expenseId) => `/expenses/${expenseId}`,
     DOWNLOAD_EXPENSE_DETAILS: "excel/download/expense",
     EMAIL_EXPENSE_DETAILS: "/email/expense-excel",
+    APPLY_FILTER: "/filter",
     UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
