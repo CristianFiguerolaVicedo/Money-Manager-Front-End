15c198b0587d3fa1568ccf8394517f03d007ba77
CriFigShe <crifigshe@gmail.com>
2025-12-22 14:25:37 +0100

    README

--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -6,7 +6,6 @@ import { API_ENDPOINTS } from "../util/apiEndpoints";
 import toast from "react-hot-toast";
 import IncomeList from "../components/IncomeList";
 import Modal from "../components/Modal";
-import { Plus } from "lucide-react";
 import AddIncomeForm from "../components/AddIncomeForm";
 import DeleteAlert from "../components/DeleteAlert";
 import IncomeOverview from "../components/IncomeOverview";
--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://money-manager-t0yw.onrender.com/api/v1.0";
-//export const BASE_URL = "http://localhost:8080/api/v1.0";
+//export const BASE_URL = "https://money-manager-t0yw.onrender.com/api/v1.0";
+export const BASE_URL = "http://localhost:8080/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "drsuueqhd";
 
 export const API_ENDPOINTS = {
